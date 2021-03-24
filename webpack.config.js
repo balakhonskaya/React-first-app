ea27be779ae938727439ab20d867f46e83b749da
Elizaveta <anthemion@yandex.ru>
2021-03-24 21:25:37 +0100

    Add dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 const NODE_ENV = process.env.NODE_ENV;
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const IS_DEV = NODE_ENV === 'development';
+const IS_PROD = NODE_ENV === 'production';
+
+function setupDevtool() {
+    if (IS_DEV) return 'eval';
+    if (IS_PROD) return false;
+}
 
 module.exports = {
     resolve: {
