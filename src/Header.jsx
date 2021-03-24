dc516b22b261c45375baff04f2414d75717c382d
Elizaveta <anthemion@yandex.ru>
2021-03-24 13:57:06 +0100

    Add jsx files, add node_modules, change index.html

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export function Header() {
+    return(
+        <Header>
+            <h1>
+                Hello React!
+            </h1>
+        </Header>
+    );
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+window.addEventListener(type:'load', listener:() => {
+    ReactDOM.render(<Header />, document.getElementById( elementId: 'react_root'));
+});
+
+
